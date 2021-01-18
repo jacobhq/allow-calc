53530c8bb7117c182d0ff4842f032d0dfc7c931b
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-18 14:44:55 +0000

    Tidy code

--- src/inject.js
+++ src/inject.js
@@ -1,16 +1,9 @@
-// this is the code which will be injected into a given page...
-
 (function() {
-
-	// just place a div at top right
-	function removeElements() {
+	function init() {
         const icon = document.querySelector('.hm-calc');
         icon.classList.remove('red');
         icon.classList.add('green');
         document.body.innerHTML = document.body.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
     }
-    removeElements()
-
-	alert('inserted self... giggity');
-
+    init()
 })();
