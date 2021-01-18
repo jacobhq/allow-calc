88f953b9713acd4fffbcb33c6832aba5a89851a3
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-18 11:44:19 +0000

    Add new page detection

--- src/background.js
+++ src/background.js
@@ -3,5 +3,5 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 		chrome.tabs.executeScript(tab.ib, {
 			file: 'inject.js'
-    }
+    })
 })
