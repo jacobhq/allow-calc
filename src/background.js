161a81d8f8b39739b31abcf897061c40838b196e
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-18 10:44:10 +0000

    Update bgjs

--- src/background.js
+++ src/background.js
@@ -1,16 +1,7 @@
 // listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
-	var hegarty = false;
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        alert(tabs[0]);
-    });
-	if (hegarty) {
 		chrome.tabs.executeScript(tab.ib, {
 			file: 'inject.js'
-		});
-	}
-	else {
-		alert('You need to be in a hegartymaths assesment to use this extension.');
-	}
-});
+    }
+})
