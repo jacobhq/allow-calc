ad90740bef4f1fcff32a6b88d5fb3e1b5ce23d7f
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-18 09:31:12 +0000

    Eslint and permissions

--- src/background.js
+++ src/background.js
@@ -1,18 +1,18 @@
 // listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
-    // for the current tab, inject the "inject.js" file & execute it
-    var hegarty = true
-    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-        function(tabs){
-            hegarty = true
-        }
-    );
-	if (hegarty = true) {
-        chrome.tabs.executeScript(tab.ib, {
-		    file: 'inject.js'
-	    });
-    }
-    else {
-        alert("You need to be in a hegartymaths assesment to use this extension.")
-    }
+	// for the current tab, inject the "inject.js" file & execute it
+	var hegarty = true;
+	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+		function(tabs){
+			hegarty = true;
+		}
+	);
+	if (hegarty) {
+		chrome.tabs.executeScript(tab.ib, {
+			file: 'inject.js'
+		});
+	}
+	else {
+		alert('You need to be in a hegartymaths assesment to use this extension.');
+	}
 });
