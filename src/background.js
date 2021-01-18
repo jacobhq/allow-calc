51f1a722442bc549d18247761bbaf3abf0112cae
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-18 10:09:01 +0000

    Try getting current tab

--- src/background.js
+++ src/background.js
@@ -1,12 +1,10 @@
 // listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
-	var hegarty = true;
-	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-		function(tabs){
-			hegarty = true;
-		}
-	);
+	var hegarty = false;
+	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        alert(tabs[0]);
+    });
 	if (hegarty) {
 		chrome.tabs.executeScript(tab.ib, {
 			file: 'inject.js'
