7b8bef0f5d2ede0723037c972c40decfc1c0e43b
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-28 11:52:43 +0000

    Use listItem.innerHTML

--- src/inject.js
+++ src/inject.js
@@ -1,15 +1,16 @@
 (function() {
 	function init() {
-        // Change icon
-        const icon = document.querySelector('.hm-calc');
-        icon.classList.remove('red');
-        icon.classList.add('green');
+        // Change icon (Not needed)
+        // const icon = document.querySelector('.hm-calc');
+        // icon.classList.remove('red');
+        // icon.classList.add('green');
 
 	    // Not using this as hegarty disables the input. More info in issues #1 and #2.
         // document.body.innerHTML = document.body.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
 
         // Change calculator text
         const listItem = document.getElementByClassName("Assessment__meta").firstChild;
+        listItem.innerHTML('<i class="hm hm-calc green"></i>You may use a calculator')
     }
     init()
 })();
