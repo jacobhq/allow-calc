822a4f8af276023e34c71ddd349a6dfe34822abb
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-01-28 11:16:13 +0000

    Comment out buggy code

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,8 @@
         const icon = document.querySelector('.hm-calc');
         icon.classList.remove('red');
         icon.classList.add('green');
-        document.body.innerHTML = document.body.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
+	// Not using this as hegarty disables the input. More info in issues #1 and #2
+        // document.body.innerHTML = document.body.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
     }
     init()
 })();
