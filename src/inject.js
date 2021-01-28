3d72c6e1bd28af9d34bd24e807a7791a3eacc620
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-28 11:43:17 +0000

    Comments and new method

--- src/inject.js
+++ src/inject.js
@@ -1,10 +1,15 @@
 (function() {
 	function init() {
+        // Change icon
         const icon = document.querySelector('.hm-calc');
         icon.classList.remove('red');
         icon.classList.add('green');
-	// Not using this as hegarty disables the input. More info in issues #1 and #2
+
+	    // Not using this as hegarty disables the input. More info in issues #1 and #2.
         // document.body.innerHTML = document.body.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
+
+        // Change calculator text
+        const listItem = document.getElementByClassName("Assessment__meta").firstChild;
     }
     init()
 })();
