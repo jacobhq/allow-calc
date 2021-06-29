1d037f18a759a601abd7e3e0718fc9d001a18440
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-06-29 10:51:42 +0100

    Scope replace

--- src/inject.js
+++ src/inject.js
@@ -1,9 +1,10 @@
 (function() {
 	function init() {
         const icon = document.querySelector('.hm-calc');
+	const ul = document.querySelector('#content > div.grey-bg > div > div > div.col-sm-3.md\:flex.flex-col > ul > li:nth-child(1)')
         icon.classList.remove('red');
         icon.classList.add('green');
-        document.body.innerHTML = document.body.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
+        ul.innerHTML = ul.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
     }
     init()
 })();
