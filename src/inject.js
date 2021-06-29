b7390b2bb943e1ecdadac75ffc0e8fb6124dd7a1
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-06-29 11:05:28 +0100

    New selector

--- src/inject.js
+++ src/inject.js
@@ -1,7 +1,7 @@
 (function() {
 	function init() {
         const icon = document.querySelector('.hm-calc');
-	const ul = document.querySelector('#content > div.grey-bg > div > div > div.col-sm-3.md\:flex.flex-col > ul > li:nth-child(1)')
+	const ul = document.querySelector('#content div.grey-bg div div div.col-sm-3.md\:flex.flex-col ul li:nth-child(1)')
         icon.classList.remove('red');
         icon.classList.add('green');
         ul.innerHTML = ul.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
