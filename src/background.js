659a517e5c9d22e15246d71a4ed8ba61ee5a502f
jacobhq <jacob.marshall.jem@gmail.com>
2021-07-02 13:52:34 +0000

    Canary clarity

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 // listen for our browerAction to be clicked
+chrome.browserAction.setBadgeText({text: 'CANARY'});
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 		chrome.tabs.executeScript(tab.ib, {
