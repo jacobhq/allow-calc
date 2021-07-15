98f60ae03af37f7dbfca12a3538df09a15f7f68f
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-07-15 20:04:46 +0100

    minor(Some work on state)

--- src/inject.js
+++ src/inject.js
@@ -1,10 +1,11 @@
-(function() {
-	function init() {
-        const icon = document.querySelector('.hm-calc');
-	const ul = document.querySelector('ul.Assessment__meta li:nth-child(1)')
-        icon.classList.remove('red');
-        icon.classList.add('green');
-        ul.innerHTML = ul.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
-    }
-    init()
+(function () {
+        function init() {
+                const icon = document.querySelector('.hm-calc');
+                const ul = document.querySelector('ul.Assessment__meta li:nth-child(1)')
+                icon.classList.remove('red');
+                icon.classList.add('green');
+                ul.innerHTML = ul.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
+        }
+
+        init()
 })();
