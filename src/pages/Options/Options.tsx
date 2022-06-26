b87530173a359e1240cac80c80a0d04d9d140681
Jacob Marshall <jacob@omg.lol>
2022-06-26 09:04:07 +0100

    ✨ Settings page

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, ChakraProvider, Container, Divider, FormControl, FormLabel, Heading, HStack, Switch, Text } from '@chakra-ui/react'
 import './Options.css';
 
 interface Props {
@@ -6,7 +7,23 @@ interface Props {
 }
 
 const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+  return <ChakraProvider>
+    <Container maxW="container.sm">
+      <Box mt={8}>
+        <Heading>{title}</Heading>
+        <Text mt={2} fontSize={16}>Configure allow-calc here.</Text>
+      </Box>
+      <Divider my={6} />
+      <Box mt={6}>
+        <FormControl>
+          <HStack justifyContent="space-between">
+            <FormLabel>Enable extension</FormLabel>
+            <Switch />
+          </HStack>
+        </FormControl>
+      </Box>
+    </Container>
+  </ChakraProvider>;
 };
 
 export default Options;
