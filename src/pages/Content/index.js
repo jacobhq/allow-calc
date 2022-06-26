db8e66ca61967bb97ed5d26e7a7f7c4ac3ba3949
Jacob Marshall <jacob@omg.lol>
2022-06-26 11:20:53 +0100

    🚩 Enable or disable inject

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,13 @@
-import { printLine } from './modules/print';
+function init() {
+    const icon = document.querySelector('.hm-calc');
+    const ul = document.querySelector('ul.Assessment__meta li:nth-child(1)')
+    icon.classList.remove('red');
+    icon.classList.add('green');
+    ul.innerHTML = ul.innerHTML.replace(/Do not use a calculator/g, "You may use a calculator");
+}
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
+chrome.storage.sync.get(['enabled'], function (result) {
+    let isTrueSet = (result.enabled === 'true')
+    console.log(isTrueSet)
+    if (isTrueSet) return init()
+});
