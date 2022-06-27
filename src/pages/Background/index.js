32d247aef7c413c7ae8698702a2972fc133c669e
Jacob Marshall <jacob@omg.lol>
2022-06-27 19:37:59 +0100

    💬 Update console message

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,1 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+console.log('[allow-calc] - Allow calc is running, go to https://hegartymaths.com, and do a quiz to see it in action.');
